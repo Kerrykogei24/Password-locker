02f2efa4d2850ed32984e99c482b3b65000c20a5
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-21 20:36:29 +0300

    add function that creates new credentials

--- run.py
+++ run.py
@@ -13,3 +13,23 @@ def save_user(user):
     function that saves a new user
     '''
     user.save_user()
+
+def display_user(user):
+    '''
+    function that displays user
+    '''
+    return User.display_user()
+
+def login_user(password,user_name):
+    '''
+    a fumction that checks if the users already exist 
+    '''
+    checked_user = Creditials.verify_user(password,user_name)
+    return checked_user
+
+def create_new_credential(f_username,f_password):
+    '''
+    function that create new credential details for a new user
+    '''
+    new_credential = Creditials(f_username,f_password)
+    return new_credential
