0c3f97b2859792cd674233800083d7a93251c1f8
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-21 20:19:54 +0300

    add password thatd creates users password

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3.6
 from user import User,Creditials
 
+
+def create_new_user(user_name,password):
+    '''
+    function that creates a user using a password and username
+    '''
+    new_user = User(user_name,password) 
+    return new_user
+def save_user(user):
+    '''
+    function that saves a new user
+    '''
+    user.save_user()
