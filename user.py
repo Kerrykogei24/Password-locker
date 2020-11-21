5a666f16e2e3c93b26d6e35ee55273f9d641564d
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-21 18:44:04 +0300

    add save function

--- user.py
+++ user.py
@@ -13,3 +13,10 @@ class User:
     def __init__(self,user_name,password):
         self.user_name = user_name
         self.password  = password
+
+    def save_user(self):
+        """
+        save_user method saves a new user object to contact list
+        """
+
+        User.user_list.append(self)
