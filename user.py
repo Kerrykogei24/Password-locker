9279b8a33a8ffe4adba9ff02fe8969bcfa8c50d6
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-21 18:36:18 +0300

    create user class and constractor

--- user.py
+++ user.py
@@ -1,2 +1,15 @@
 import random
 import string
+
+class User:
+
+    """
+    class that genarate intance of user
+    user_list password
+    """
+
+    user_list = []
+
+    def __init__(self,user_name,password):
+        self.user_name = user_name
+        self.password  = password
