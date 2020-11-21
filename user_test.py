728e815e7dbf967b8730c04fa088e9f84ed9debc
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-21 19:10:30 +0300

    test case to check if the object in initialized correctly

--- user_test.py
+++ user_test.py
@@ -9,3 +9,10 @@ class TestClass(unittest.TestCase):
         a method that runs before the test
         """
         self.new_user = User("kerry","joker1234")
+
+    def test_init(self):
+        """
+        test case to check if the object in initialized correctly
+        """
+        self.assertEqual(self.new_user.user_name,'kerry')
+        self.assertEqual(self.new_user.password,'joker1234')
