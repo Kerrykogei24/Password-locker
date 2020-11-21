2cbe56de29d8a90b718cd719fda2366e035f4d83
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-21 19:09:31 +0300

    add a method that runs before the test

--- user_test.py
+++ user_test.py
@@ -1,3 +1,11 @@
 import unittest
 from user import User
 from user import Creditials
+
+class TestClass(unittest.TestCase):
+  
+    def setUp(self):
+        """
+        a method that runs before the test
+        """
+        self.new_user = User("kerry","joker1234")
