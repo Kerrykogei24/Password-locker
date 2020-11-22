87030ce47248c549f38232eece454c8f66d2b8bb
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-22 14:57:15 +0300

    add

--- user.py
+++ user.py
@@ -11,7 +11,7 @@ class User:
     user_list = []
 
     def __init__(self,username,password):
-        self.user_name = username
+        self.username = username
         self.password  = password
 
     def save_user(self):
